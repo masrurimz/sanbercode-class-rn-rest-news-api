31cfebe016d30468866622105c62c94b72ada351
Muhammad Zahid Masruri <masruri03@gmail.com>
2021-12-19 22:27:35 +0700

    standaraize response

--- routes/news.js
+++ routes/news.js
@@ -1,16 +1,37 @@
 const express = require("express");
+
+const {
+	error: errorMessage,
+	success,
+	validation,
+} = require("../models/responseApi");
 const router = express.Router();
 
 const { News, validate } = require("../models/news");
 
 router.get("/", async (req, res) => {
 	const news = await News.find();
-	res.send({ data: news });
+	res.send(
+		success({
+			message: "OK",
+			statusCode: res.statusCode,
+			results: {
+				news,
+			},
+		}),
+	);
 });
 
 router.post("/", async (req, res) => {
 	const { error } = validate(req.body);
-	if (error) return res.status(400).send(error.details[0].message);
+	if (error)
+		return res.status(400).send(
+			errorMessage({
+				message: error.details[0].message,
+				statusCode: res.statusCode,
+				results: {},
+			}),
+		);
 
 	const news = new News({
 		title: req.body.title,
@@ -18,12 +39,27 @@ router.post("/", async (req, res) => {
 	});
 	await news.save();
 
-	res.send({ data: news });
+	res.send(
+		success({
+			message: "OK",
+			statusCode: res.statusCode,
+			results: {
+				news,
+			},
+		}),
+	);
 });
 
 router.put("/:id", async (req, res) => {
 	const { error } = validate(req.body);
-	if (error) return res.status(400).send(error.details[0].message);
+	if (error)
+		return res.status(400).send(
+			errorMessage({
+				message: error.details[0].message,
+				statusCode: res.statusCode,
+				results: {},
+			}),
+		);
 
 	const news = await News.findByIdAndUpdate(
 		req.params.id,
@@ -36,22 +72,46 @@ router.put("/:id", async (req, res) => {
 		},
 	);
 	if (!news)
-		return res
-			.status(404)
-			.send("The news with the given ID could not be found");
+		return res.status(404).send(
+			errorMessage({
+				message: "The news with the given ID could not be found",
+				statusCode: res.statusCode,
+				results: {},
+			}),
+		);
 
-	res.send({ data: news });
+	res.send(
+		success({
+			message: "OK",
+			statusCode: res.statusCode,
+			results: {
+				news,
+			},
+		}),
+	);
 });
 
 router.delete("/:id", async (req, res) => {
 	const news = await News.findByIdAndRemove(req.params.id);
 
 	if (!news)
-		return res
-			.status(404)
-			.send("The news with the given ID could not be found");
+		return res.status(404).send(
+			errorMessage({
+				message: "The news with the given ID could not be found",
+				statusCode: res.statusCode,
+				results: {},
+			}),
+		);
 
-	res.send({ data: news });
+	res.send(
+		success({
+			message: "OK",
+			statusCode: res.statusCode,
+			results: {
+				news,
+			},
+		}),
+	);
 });
 
 module.exports = router;
